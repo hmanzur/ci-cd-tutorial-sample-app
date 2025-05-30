7705cbbff58ccb962de6382d613ea0b58e0f1e28
Habid Manzur <hmanzur@atlassian.com>
2025-05-29 20:59:48 -0500

    fix(test): test according flask 3

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,7 +1,6 @@
 import os
 import sys
 import unittest
-
 import json
 
 # Add parent directory to path for import
@@ -11,7 +10,7 @@ from app import app, db
 from app.models import Menu
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-TEST_DB = os.path.join(BASE_DIR, 'app.db')
+TEST_DB = os.path.join(BASE_DIR, 'test.db')
 
 
 class BasicTests(unittest.TestCase):
@@ -20,9 +19,11 @@ class BasicTests(unittest.TestCase):
         app.config['SQLALCHEMY_DATABASE_URI'] = \
             os.environ.get('TEST_DATABASE_URL') or \
             'sqlite:///' + TEST_DB
+        app.config['TESTING'] = True  # Importante para el modo testing
         self.app = app.test_client()
-        db.drop_all()
-        db.create_all()
+        with app.app_context():
+            db.drop_all()
+            db.create_all()
 
     def tearDown(self):
         pass
@@ -31,7 +32,7 @@ class BasicTests(unittest.TestCase):
         response = self.app.get('/', follow_redirects=True)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.mimetype, 'application/json')
-        body = json.loads(response.data)
+        body = json.loads(response.data.decode('utf-8'))
         self.assertEqual(body['status'], 'ok')
 
     def test_menu_empty(self):
@@ -40,13 +41,14 @@ class BasicTests(unittest.TestCase):
 
     def test_menu_item(self):
         test_name = "test"
-        test_item = Menu(name=test_name)
-        db.session.add(test_item)
-        db.session.commit()
+        with app.app_context():
+            test_item = Menu(name=test_name)
+            db.session.add(test_item)
+            db.session.commit()
         response = self.app.get('/menu', follow_redirects=True)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.mimetype, 'application/json')
-        body = json.loads(response.data)
+        body = json.loads(response.data.decode('utf-8'))
         self.assertTrue('today_special' in body)
         self.assertEqual(body['today_special'], test_name)
 
